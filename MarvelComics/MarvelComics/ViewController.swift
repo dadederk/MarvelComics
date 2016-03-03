e94cfae0fdcbae66f8185d697082506187528eac
Daniel Devesa Derksen-Staats <dadederk@gmail.com>
2016-03-03 09:49:07 +0000

    Get comics ordered by on sale date in descending mode

--- MarvelComics/MarvelComics/ViewController.swift
+++ MarvelComics/MarvelComics/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
             }
         }
         
-        MarvelAPIClient.comics { comics -> () in
+        MarvelAPIClient.comics(orderBy:OrderBy.onsaleDescending) { comics -> () in
             
                 self.dataSource = ArrayDataSource(
                     items: comics,
