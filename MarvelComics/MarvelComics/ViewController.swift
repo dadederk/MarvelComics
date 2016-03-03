f520b452da100ee85793c26cf7a71dd3344db795
Daniel Devesa Derksen-Staats <dadederk@gmail.com>
2016-03-03 19:12:56 +0000

    Added an activity indicator to show that the app it's loading the first data

--- MarvelComics/MarvelComics/ViewController.swift
+++ MarvelComics/MarvelComics/ViewController.swift
@@ -11,34 +11,46 @@ import UIKit
 class ViewController: UIViewController {
 
     var dataSource: ArrayDataSource!
+    var configureCell: ConfigureCell!
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let configureCell: ConfigureCell = {(cell, item) in
+        self.configureCell = {(cell, item) in
             if let comic = item as? Comic,
                 let comicCell = cell as? ComicCell {
                     comicCell.titleLabel.text = comic.title
             }
         }
         
+        self.tableView.hidden = true;
+        self.initialLoading()
+    }
+    
+    func initialLoading() {
+    
         MarvelAPIClient.comics(orderBy:OrderBy.onsaleDescending) { comics -> () in
             
             if let comics = comics {
-            
+                
                 self.dataSource = ArrayDataSource(
                     items: comics,
                     cellReuseIdentifier: String(ComicCell),
-                    configureCell: configureCell)
+                    configureCell: self.configureCell)
                 
                 self.tableView?.dataSource = self.dataSource
                 self.tableView.reloadData()
+                
+                self.activityIndicator.stopAnimating()
             } else {
                 
                 print("Error getting comics")
             }
+            
+            self.tableView.hidden = false;
         }
     }
 }
